379a01ffbdeef6720daed687b93bc2327573a20f
Caleb Cyphers <caleb.j.cyphers@gmail.com>
2021-01-18 16:52:28 -0700

    Add parsing to apicalls

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,6 +1,7 @@
 const getRandomQuote = () => {
   return fetch('https://quote-garden.herokuapp.com/api/v3/quotes/random')
     .then(response => response.json())
+    .then(response => response.data[0])
 }
 
 export { getRandomQuote };
