6db2d45e9ac2d8ae8ca146587fe2af9c9a3eaa3a
Caleb Cyphers <caleb.j.cyphers@gmail.com>
2021-01-18 16:58:39 -0700

    Adjust classname from Main to Home

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Home({ quote }) {
   return (
-    <section className="Main-quote-display">
+    <section className="Home-quote-display">
       <p>{quote.quoteText}</p>
       <p>-{quote.quoteAuthor}</p>
     </section>
