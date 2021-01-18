cee33dbf88ab7a72a46e018ad541e1ebe74a8f32
Caleb Cyphers <caleb.j.cyphers@gmail.com>
2021-01-18 16:53:18 -0700

    call getquote and set the state of app

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,17 +1,36 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
-import Home from "../Home/Home"
+import Home from "../Home/Home";
+import { getRandomQuote } from "../../apiCalls";
 
 function App() {
-  const [quote, setQuote] = useState("");
+  const [quote, setQuote] = useState({
+    _id: "",
+    quoteText: "",
+    quoteAuthor: "",
+    quoteGenre: "",
+    __v: 0,
+  });
+  const [error, setError] = useState(false)
 
   const setDefaultQuote = () => {
+    getRandomQuote()
+      .then(response => setQuote(response))
+      .catch(err => setError(true));
+  };
 
-  }
+  useEffect(() => {
+    if (!quote.quoteText) {
+      setDefaultQuote();
+    }
+  });
 
-  return <div className="App">
-    <Home />
-  </div>;
+  return (
+    <div className="App">
+      {error && <h1>There was a problem loading the quote. Try again later.</h1>}
+      <Home quote={quote} />
+    </div>
+  );
 }
 
 export default App;
