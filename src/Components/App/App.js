9b8119da295400f5773cf3a421d4661beef9060f
Caleb Cyphers <caleb.j.cyphers@gmail.com>
2021-01-18 17:53:19 -0700

    Add saveQuote function and savedQuotes state

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import Home from "../Home/Home";
 import NavBar from "../NavBar/NavBar"
+import MyQuotes from "../MyQuotes/MyQuotes"
 import { Route, Switch } from "react-router-dom"
 import { getRandomQuote } from "../../apiCalls";
 
@@ -12,6 +13,7 @@ function App() {
     quoteAuthor: "",
     quoteGenre: "",
   });
+  const [savedQuotes, setSavedQuotes] = useState([])
   const [error, setError] = useState(false)
 
   const setDefaultQuote = () => {
@@ -19,6 +21,10 @@ function App() {
       .then(response => setQuote(response))
       .catch(err => setError(true));
   };
+
+  const saveQuote = (newQuote) => {
+    setSavedQuotes([...savedQuotes, newQuote])
+  }
 
   useEffect(() => {
     if (!quote.quoteText) {
@@ -31,9 +37,9 @@ function App() {
       <NavBar />
       {error && <h1>There was a problem loading the quote. Try again later.</h1>}
       <Switch>
-        <Route path="/" render={() => <Home quote={quote} />}/>
-        {/* <Route exact path="/newquote" render={} />
-        <Route exact path="/myquotes" render={} /> */}
+        <Route exact path="/" render={() => <Home quote={quote} saveQuote={saveQuote}/>}/>
+        <Route exact path="/myquotes" render={() => <MyQuotes savedQuotes={savedQuotes}/>} />
+        {/* <Route exact path="/newquote" render={} /> */}
       </Switch>
     </div>
   );
