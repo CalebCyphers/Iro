692fee41d9bbbbbcaeed310a265f5f521b765f2c
Caleb Cyphers <caleb.j.cyphers@gmail.com>
2021-01-18 19:42:56 -0700

    Add test for loader and loader

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,11 +1,10 @@
 import React from 'react'
 
 export default function Home({ quote, saveQuote, newQuote }) {
-  console.log(quote)
   return (
     <section className="Home-quote-display">
-      <p>{quote.quoteText}</p>
-      <p>-{quote.quoteAuthor}</p>
+      {quote.quoteText ? <p>{quote.quoteText}</p> : <p>Loading...</p>}
+      {quote.quoteText && <p>-{quote.quoteAuthor}</p>}
       <button onClick={() => newQuote()}>Next</button>
       <button onClick={() => saveQuote(quote)}>Save</button>
     </section>
--- src/Components/Home/Home.test.js
+++ src/Components/Home/Home.test.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { screen, render } from "@testing-library/react";
 import "@testing-library/jest-dom"
-import userEvent from "@testing-library/user-event";
+import { userEvent } from "@testing-library/user-event";
 import Home from "./Home.js";
 
 const mockSetDefaultQuote = jest.fn();
@@ -28,4 +28,47 @@ describe("Home", () => {
     expect(quoteBody).toBeInTheDocument();
     expect(quoteAuthorText).toBeInTheDocument();
   });
+
+  it("Should display a Next and Save button", () => {
+    render(
+      <Home
+        quote={mockQuote}
+        newQuote={mockSetDefaultQuote}
+        saveQuote={mockSaveQuote}
+      />
+    );
+    const saveButton = screen.getByText("Save")
+    const nextButton = screen.getByText("Next")
+
+    expect(saveButton).toBeInTheDocument()
+    expect(nextButton).toBeInTheDocument()
+  })
+
+  it("Should display a loading message if there is no quote yet", () => {
+    render(
+      <Home
+        quote={{}}
+        newQuote={mockSetDefaultQuote}
+        saveQuote={mockSaveQuote}
+      />
+    );
+    const loadingMessage = screen.getByText("Loading...")
+
+    expect(loadingMessage).toBeInTheDocument()
+  })
+
+  it("Should call a function when the Save button is clicked", () => {
+    render(<Home
+        quote={mockQuote}
+        newQuote={mockSetDefaultQuote}
+        saveQuote={mockSaveQuote}
+      />
+      );
+      const saveButton = screen.getByText("Save")
+
+      userEvent().click(saveButton)
+
+      expect(mockSaveQuote).toHaveBeenCalled()
+  })
+  userEvent()
 });
