4bcd7abb1dc3e8eaff420a3062f8e0e3a211bf72
Caleb Cyphers <caleb.j.cyphers@gmail.com>
2021-01-18 17:58:49 -0700

    Add Save button to home

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,10 +1,11 @@
 import React from 'react'
 
-export default function Home({ quote }) {
+export default function Home({ quote, saveQuote }) {
   return (
     <section className="Home-quote-display">
       <p>{quote.quoteText}</p>
       <p>-{quote.quoteAuthor}</p>
+      <button onClick={() => saveQuote(quote)}>Save</button>
     </section>
   );
 }
