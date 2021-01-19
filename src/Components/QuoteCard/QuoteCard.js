42a92f4ffdbf20855d3358d0de55fac87e6e8b96
Caleb Cyphers <caleb.j.cyphers@gmail.com>
2021-01-19 13:45:26 -0700

    Add PropTypes to QuoteCard

--- src/Components/QuoteCard/QuoteCard.js
+++ src/Components/QuoteCard/QuoteCard.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types';
 
 export default function QuoteCard({ quote }) {
   return (
@@ -7,4 +8,8 @@ export default function QuoteCard({ quote }) {
       <p>-{quote.quoteAuthor}</p>
     </div>
   )
+}
+
+QuoteCard.propTypes = {
+  quote: PropTypes.object
 }
