acfc8909cbe5cde36f43aca81e13f49008c3dccb
Caleb Cyphers <caleb.j.cyphers@gmail.com>
2021-01-18 20:21:17 -0700

    Adjust MyQuotes tests

--- src/Components/MyQuotes/MyQuotes.test.js
+++ src/Components/MyQuotes/MyQuotes.test.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { screen, render } from "@testing-library/react";
 import "@testing-library/jest-dom";
-import userEvent from "@testing-library/user-event";
 import MyQuotes from "./MyQuotes.js";
 
 const mockSavedQuotes = [
