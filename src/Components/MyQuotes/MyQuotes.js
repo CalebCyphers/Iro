9c22a7bcd5b62ff4387c2b5de7028bf10d055baf
Caleb Cyphers <caleb.j.cyphers@gmail.com>
2021-01-18 18:20:48 -0700

    add buildQuoteCards function

--- src/Components/MyQuotes/MyQuotes.js
+++ src/Components/MyQuotes/MyQuotes.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import QuoteCard from '../QuoteCard/QuoteCard'
 
 export default function MyQuotes({ savedQuotes }) {
+
+  const buildQuoteCards = (quotes) => {
+    return quotes.map(quote => <QuoteCard quote={quote} />)
+  }
+
   return (
     <section>
       {!savedQuotes[0] && <p>You haven't saved any quotes... once you do, they'll show up here.</p>}
-      <p>{savedQuotes[0]}</p>
+      <p>{buildQuoteCards(savedQuotes)}</p>
     </section>
   )
 }
